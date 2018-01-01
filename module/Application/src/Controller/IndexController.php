56b9ac2c63c8b32bd813a4ba86ffa65f08dcfd72
Yoann <contact@yoannvie.fr>
2018-01-01 19:42:50 +0100

    #all : reformat native controller class

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -1,18 +1,23 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
+
+declare(strict_types=1);
 
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+/**
+ * Class IndexController
+ * @package Application\Controller
+ */
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
+
+    /**
+     * @return ViewModel
+     */
+    public function indexAction() : ViewModel
     {
         return new ViewModel();
     }
